3f45b3292a595cc51745f1a078ce6c2a755ea1af
theodorCodes <laphan.fan@gmail.com>
2021-12-02 20:37:32 +0800

    Added bag session to contexts.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,41 +1,63 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404  # product shortcut
+from products.models import Product  # product from model
 
 
 def bag_contents(request):
     """ Handles bag items, registrated in settings.py 
     for project wide availability """
 
-    # Variable bag_items stores bag items
-    bag_items = []
+    # 1) Variable bag_items stores bag items
+    # 2) Variable total stores sum of all product prices
+    # 3) Variable product_count stores number of items
+    # 5) (session) Adding session 'bag' from bag/views.py here
 
-    # Variable total stores sum of all product prices
-    total = 0
+    bag_items = []  # 1)
+    total = 0  # 2)
+    product_count = 0  # 3)
+    bag = request.session.get('bag', {})  # 5)
 
-    # Variable product_count stores number of items
-    product_count = 0
 
-    # Calculating when free shipping is applicable
-    # Percentage based 'delivery' cost calculation
-    # Assumption and current setting:
-    # FREE_DELIVERY_THRESHOLD = 50
-    # STANDARD_DELIVERY_PERCENTAGE = 10 
-    # Calculate shipping when total is under 50
+    # 6) (session) Extract data for each item in bag variable
+    # Get product
+    # Calc total + quantity * price
+    # Increment product_count variable by the quantity
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+
+    # Then store variables in bag_items including the product itself
+    bag_items.append({
+        'item_id': item_id,
+        'quantity': quantity,
+        'product': product,
+    })
+
+    # 4) Percentage based 'delivery' cost calculation
+    # a) Calculate shipping when total is under 50 (see settings in settings.py)
+    # b) Estimate 'delivery' (total * 10 / 100)
+    # c) Estimate missing sum to reach free delivery
+    # d) Delivery is free when total is greater than 50
+    # e) Grand total or final sum to be charged
+    # f) Making the following data avaible or accessible from other templates
+    #    as this file is registered in project_main/settings.py
+    # g) return values
+
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        # Estimate shipping cost
+        # b)
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        # Estimate missing sum to reach free delivery
+        # c)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-    # When total is greater then 50
     else:
-        # Delivery is free
+        # d)
         delivery = 0
         free_delivery_delta = 0
-    # Grand total or final sum to be charged
+    # e)
     grand_total = delivery + total
 
-    # This file is registered in project_main/settings.py
-    # making the following data avaible or accessible from other templates
+    # f)
     context = {
         'bag_items': bag_items,
         'total': total,
@@ -46,4 +68,5 @@ def bag_contents(request):
         'grand_total': grand_total,
     }
 
+    # g)
     return context
