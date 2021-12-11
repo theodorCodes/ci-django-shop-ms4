076bad1eb1bcd14d3709dee4c183a89830699cc1
theodorCodes <laphan.fan@gmail.com>
2021-12-11 16:01:14 +0800

    Added theme to allauth templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404  # Import products
+from .models import UserProfile  # Import profiles from models
+
 
 def profile(request):
     """ Display user profile """
+    # Return profile to template
+    profile = get_object_or_404(UserProfile, user=request.user)
     # Store template
     template = 'profiles/profile.html'
+    # Add profile to context
     context = {
-        
+        'profile': profile,
     }
     return render(request, template, context)
