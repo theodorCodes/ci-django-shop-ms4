1e5ffee3df7e9e80a15f82d13163337fbaaeb63a
theodorCodes <laphan.fan@gmail.com>
2021-12-21 16:57:19 +0800

    Added email settings

--- project_main/settings.py
+++ project_main/settings.py
@@ -248,3 +248,18 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')  # Stripe webhook secret
 DEFAULT_FROM_EMAIL = 'bettertryout@gmail.com'  # Use in webhook_hander
+
+
+# Using Gmail
+# New email settings
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'milestone4@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
